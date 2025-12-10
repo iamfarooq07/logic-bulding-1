bc5771688ad64ae56401b6db382d05c189c7562e
Muhammad Farooq <fm6182218@gmail.com>
2025-12-10 22:44:54 +0500

    LeedCode Question

--- script.js
+++ script.js
@@ -426,3 +426,245 @@
 // let b = 30;
 // let c = a + b;
 // console.log(`The Sum is ${c}`);
+
+
+// let user = prompt("Enter Your Number");
+
+// function name() {
+//     if (user % 2 === 0) {
+//         console.log(user + "" + "is Even");
+
+//     } else {
+//         console.log(user + "" + "is Odd");
+
+//     }
+// }
+// name()
+
+// Ek variable max rakho
+// Loop chalao
+// Har element ko compare karo
+// let max = [10, 20, 50, 60, -1, 100, 500];
+
+// let biggest = max[0];
+
+// max.forEach(val => {
+//     // console.log(val);
+
+//     if (biggest < val) {
+//         biggest = val
+//     }
+// })
+
+// console.log(biggest);
+
+// let multipl = (a, b) => a + b;
+// let result = multipl(10, 20);
+// console.log(result)
+
+// function multiply(a, b) {
+//     return a + b
+// }
+// let result = multiply(10, 20);
+// console.log(result);
+
+// let multiply = (a, b) => a + b;
+// let result = multiply(10, 20);
+// console.log(result);
+
+// try {
+//     console.log('abc');
+//     console.log('def');
+// } catch (error) {
+//     console.log(error.message); 
+// }
+
+// console.log(100 / 10); // -> Infinity
+// console.log(100 * "2"); // -> 200
+// console.log(100 * "2");
+
+// let sX = prompt("Enter the first number");
+// let sY = prompt("Enter the second number");
+// let x = Number(sX);
+// let y = Number(sY);
+// if (Number.isFinite(x) && Number.isFinite(y) && y !== 0) {
+//     console.log(x / y);
+// } else {
+//     console.log("incorrect arguments");
+// }
+
+// let arr = ["farooq", "umer", "ali"];
+// arr.push("game");
+// console.log(arr);
+
+// let a = 20 + "10";
+// let b = 20 + +"10";
+// let c = 20 + -"10" + "10";
+// let d = "10" - "10" + "100";
+// let e = "A" - "B" + 0xA;
+
+// let min = temp[0]
+
+// temp.forEach(e => min = min > e ? e : min);
+// console.log(min);
+
+// let x = [10, 20, 30, 40];
+// let y = [50, 60];
+// x.reverse().push(y);
+// console.log(x.length);
+
+// let route = {distance: 131, elevation: 1.4};
+// for (let k in route) console.log(k);
+
+// let colors = ['red', 'green', 'blue'];
+// for (let c of colors) console.log(c);
+
+// let a = (n) => {
+//     return n > 2 ? n * a(n - 1) : 2
+// }
+// console.log(a(6));
+
+// function execute(todo, a, b) {
+// return todo(a, b);
+// }
+// console.log(execute(power, 3, 2));
+
+
+// shallow copy and deep copy
+
+let orignalObj = {
+    name: "farooq",
+    age: 23,
+    address: { city: "Karachi" },
+}
+// console.log(orignalObj);
+
+let shallowCopy = { ...orignalObj }
+let deepcopy = JSON.parse(JSON.stringify(orignalObj));
+
+// console.log(shallowCopy);
+
+// orignalObj.address.city = "lahore"
+// console.log(orignalObj);
+// console.log(shallowCopy);
+// console.log(deepcopy);
+
+// ==============================
+// LeedCode Question and Answer //
+
+// Reverse an Array
+
+// let arr = [1, 2, 3, 4];
+// let reverseArray = arr.reverse();
+// console.log(reverseArray); // output is [4, 3, 2, 1]
+
+// Without Method
+
+// function reverse(arr) {
+//     let num = [];
+
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         num.push(arr[i])
+//     }
+//     return num
+// }
+// console.log(reverse([1, 2, 3, 4]));
+
+
+// Sum of all elements in an array
+
+// function arrSum() {
+//     let sum = 0;
+//     let arr = [1, 2, 3, 4];
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i]
+//     }
+//     return sum
+// }
+// arrSum()
+
+// Check if a string is palindrome
+
+// function isPalindrome(str) {
+//     let start = 0;
+//     let end = str.length - 1;
+
+//     while (start < end) {
+//         if (str[start] !== str[end]) {
+//             return false;
+//         }
+//         start++;
+//         end--;
+//     }
+
+//     return true;
+// }
+// console.log(isPalindrome("level"));
+// console.log(isPalindrome("hello"));
+
+// interview question
+
+// function isAnagram(str1, str2) {
+//     let one = str1.toLowerCase().split("");
+//     let two = str2.toLowerCase().split("");
+
+//     one.sort((a, b) => a.localeCompare(b));
+//     two.sort((a, b) => a.localeCompare(b));
+
+//     if (one.join("") === two.join("")) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+
+//     }
+//     return { one, two }
+// }
+// isAnagram("listen", "silent")
+// isAnagram("hello", "world")
+
+
+// let arr = [10, "amin", "farooq", 8, 7];
+
+// let num = [];
+// let str = [];
+
+// arr.forEach((value) => {
+//     if (typeof value == "number") num.push(value);
+//     else if (typeof value == "string") str.push(value)
+// })
+// console.log(num);
+// console.log(str);
+
+// closure function in javascript
+
+// function createCounter() {
+//     let count = 0;
+//     function inner() {
+//         count += 1
+//         return count
+//     }
+//     return inner
+// }
+// const counter = createCounter();
+// console.log(counter()); // 1
+// console.log(counter()); // 2
+
+// String first Char Capital
+
+// let str = "hello iam farooq"
+// let result = str[0].toUpperCase() + str.slice(1)
+// let change = str.slice(1);
+// // console.log(result, str);
+// console.log(result);
+
+
+
+// ==============================
+
+
+
+
+
+
+
+
